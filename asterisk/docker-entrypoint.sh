848f59f301761a78bed40abb6087b7dfa7c52d2c
yaroff <vladyslav.iarovyi@nure.ua>
2026-03-02 22:47:13 +0200

    fix: fixed res_pgsql config name

--- asterisk/docker-entrypoint.sh
+++ asterisk/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 sed "s/\${POSTGRES_PASSWORD}/$POSTGRES_PASSWORD/g" \
-    /etc/asterisk/res_pgsql.conf > /tmp/res_pgsql.conf
+    /etc/asterisk/res_config_pgsql.conf > /tmp/res_config_pgsql.conf
 
-cp /tmp/res_pgsql.conf /etc/asterisk/res_pgsql.conf
+cp /tmp/res_config_pgsql.conf /etc/asterisk/res_config_pgsql.conf
 
 exec asterisk -f
