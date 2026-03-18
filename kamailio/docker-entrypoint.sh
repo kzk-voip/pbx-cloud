22560585b67cc167ce94bbdb02b97dbe4933ff3b
yaroff <vladyslav.iarovyi@nure.ua>
2026-03-19 00:09:32 +0200

    modified the entrypoint to copy the config to a temporary location

--- kamailio/docker-entrypoint.sh
+++ kamailio/docker-entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
 
-# Fix Windows CRLF line endings for mounted config
-for f in /etc/kamailio/*.cfg; do
-    [ -f "$f" ] || continue
-    sed 's/\r$//' "$f" > "/tmp/$(basename "$f")"
-    cp "/tmp/$(basename "$f")" "$f"
-done
+# Copy mounted config to a writable location
+cp /etc/kamailio/kamailio.cfg /tmp/kamailio.cfg
 
-# Substitute environment variables in kamailio.cfg
-sed -i "s|\${ASTERISK_IP}|${ASTERISK_IP:-127.0.0.1}|g" /etc/kamailio/kamailio.cfg
-sed -i "s|\${ASTERISK_PORT}|${ASTERISK_PORT:-5070}|g" /etc/kamailio/kamailio.cfg
-sed -i "s|\${EXTERNAL_IP}|${EXTERNAL_IP:-127.0.0.1}|g" /etc/kamailio/kamailio.cfg
+# Fix Windows CRLF line endings
+sed -i 's/\r$//' /tmp/kamailio.cfg
 
-exec kamailio -DD -E -m 64 -M 8
+# Substitute environment variables
+sed -i "s|\${ASTERISK_IP}|${ASTERISK_IP:-127.0.0.1}|g" /tmp/kamailio.cfg
+sed -i "s|\${ASTERISK_PORT}|${ASTERISK_PORT:-5070}|g" /tmp/kamailio.cfg
+sed -i "s|\${EXTERNAL_IP}|${EXTERNAL_IP:-127.0.0.1}|g" /tmp/kamailio.cfg
+
+exec kamailio -DD -E -m 64 -M 8 -f /tmp/kamailio.cfg
